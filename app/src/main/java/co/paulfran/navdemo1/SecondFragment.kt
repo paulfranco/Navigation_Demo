ee622fd9d86f6f2c90f202b2a05cd7db0314b666
Paul Franco <mail@paulfran.co>
2020-08-26 02:11:00 -0700

    Initial commit

---
+++ app/src/main/java/co/paulfran/navdemo1/SecondFragment.kt
@@ -0,0 +1,29 @@
+package co.paulfran.navdemo1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import co.paulfran.navdemo1.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+
+    private lateinit var binding: FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false)
+
+        var input: String? = requireArguments().getString("user_input")
+
+        binding.textView.text = input.toString()
+        return binding.root
+    }
+
+
+}
